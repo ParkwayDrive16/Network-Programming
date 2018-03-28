de214362d9502f729c74c54ad359c91966e24ad9
Kyrylo Melnychuk <mrtrosha.au@gmail.com>
2018-03-28 13:59:33 +1100

    Added comments

--- Lab 4-5/Task 3/TCPClient.java
+++ Lab 4-5/Task 3/TCPClient.java
@@ -16,27 +16,32 @@ class TCPClient{
 	PrintStream outChSum = null;
 	BufferedReader inText = null;
 
+	//creating sockets, streams and buffered reader
 	textSocket = new Socket(serverHostname, textPort);
 	checksumSocket = new Socket(serverHostname, checksumPort);
 	outText = new PrintStream(textSocket.getOutputStream(), true);
 	outChSum = new PrintStream(checksumSocket.getOutputStream(), true);
 	CheckedOutputStream csum = new CheckedOutputStream(outText, new Adler32());
 	inText = new BufferedReader(new InputStreamReader(textSocket.getInputStream()));
-
 	BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 	String userInput;
 
+	//loops to get user input untill user input in null
 	while ((userInput = stdIn.readLine()) != null) {
+		//adds a new line character to the input, converts to byte array and writes to output stream
 		userInput += "\n";
 		byte[] b = userInput.getBytes();
 		csum.write(b);
+		//making sure to flush the output stream before accessing checksum
 		csum.flush();
-
+		//sends checksum value to the server
 		outChSum.println(csum.getChecksum().getValue());
+		//prints the received message from the server
 		System.out.println("echo: " + inText.readLine());
+		//resets the checksum after all operations
 		csum.getChecksum().reset();
 	}
-
+	//closing all streams and sockets
 	csum.close();
 	outText.close();
 	outChSum.close();
--- Lab 4-5/Task 3/TCPServer.java
+++ Lab 4-5/Task 3/TCPServer.java
@@ -13,12 +13,12 @@ class TCPServer{
 	ServerSocket checksumSocket = null;
 	Socket clientSocket = null;
 	Socket clientSocket2 = null;
-
+	//creating server sockets, connection listeners
 	communicationSocket = new ServerSocket(textPort);
 	checksumSocket = new ServerSocket(checksumPort);
 	clientSocket = communicationSocket.accept();
 	clientSocket2 = checksumSocket.accept();
-
+	//creating streams and buffered reader
 	PrintStream out = new PrintStream(clientSocket.getOutputStream(), true);
 	CheckedInputStream csum = new CheckedInputStream(clientSocket.getInputStream(), new Adler32());
 	BufferedReader inText = new BufferedReader( new InputStreamReader(csum));
@@ -26,34 +26,32 @@ class TCPServer{
 	
 	String inputText;
 	String inputCheckSum;
-
+	//loop that runs unless 2 strings received are null
 	while ((inputText = inText.readLine()) != null && (inputCheckSum = inCheckSum.readLine()) != null) {
+		//converting checksum from long to a string
 		String calculatedCsum = Long.toString((csum.getChecksum().getValue()));
+		//displaying received and calculated checksums
 		System.out.println ("Received checksum: \t" + inputCheckSum); 
 		System.out.println("Calculated checksum: \t" + calculatedCsum);
-
+		//compares checksums and writes a message accordingly
 		if(inputCheckSum.equals(calculatedCsum))
 			System.out.println("Matched!");
 		else
 			System.out.println("Oops, didn't match!");
-
+		//sends received message back to the client
 		out.println(inputText);
+		//resets the checksum after all operations
 		csum.getChecksum().reset();
+		//exit if received X from the client
 		if (inputText.equals("X")) break; 
 	}
-
-	out.close(); 
+	//closing all streams and sockets
+	out.close();
 	inText.close();
 	inCheckSum.close();
 	clientSocket.close();
 	clientSocket2.close();
 	communicationSocket.close();
 	checksumSocket.close();
-	}
-
-	public boolean areTheSame(String string1, String string2){
-		if(string1.equals(string2))
-			return true;
-		else return false;
 	}
 }
