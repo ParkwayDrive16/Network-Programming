77f2148b03e60e044811db0d715d27413924713b
Kyrylo Melnychuk <mrtrosha.au@gmail.com>
2018-05-02 12:10:41 +1000

    lab 8-9 task 1 complete

--- Lab 4-5/Task 1/ListNIFs.java
+++ Lab 4-5/Task 1/ListNIFs.java
@@ -5,49 +5,49 @@ import static java.lang.System.out;
 
 public class ListNIFs 
 {
-    public static void main(String args[]) throws SocketException {
-        Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-        
-        for (NetworkInterface netIf : Collections.list(nets)) {
-            out.printf("Display name: %s\n", netIf.getDisplayName());
-            out.printf("Name: %s\n", netIf.getName());
-            displaySubInterfaces(netIf);
-            displayInterfaceInformation(netIf);
-            displayMacAddresses(netIf);
-            out.printf("\n");
-        }
-    }
+	public static void main(String args[]) throws SocketException {
+		Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+		
+		for (NetworkInterface netIf : Collections.list(nets)) {
+			out.printf("Display name: %s\n", netIf.getDisplayName());
+			out.printf("Name: %s\n", netIf.getName());
+      displaySubInterfaces(netIf);
+			displayInterfaceInformation(netIf);
+			displayMacAddresses(netIf);
+			out.printf("\n");
+		}
+	}
 
-    static void displaySubInterfaces(NetworkInterface netIf) throws SocketException {
-        Enumeration<NetworkInterface> subIfs = netIf.getSubInterfaces();
-        
-        for (NetworkInterface subIf : Collections.list(subIfs)) {
-            out.printf("\tSub Interface Display name: %s\n", subIf.getDisplayName());
-            out.printf("\tSub Interface Name: %s\n", subIf.getName());
-        }
-     }
+	static void displaySubInterfaces(NetworkInterface netIf) throws SocketException {
+		Enumeration<NetworkInterface> subIfs = netIf.getSubInterfaces();
+		
+		for (NetworkInterface subIf : Collections.list(subIfs)) {
+			out.printf("\tSub Interface Display name: %s\n", subIf.getDisplayName());
+			out.printf("\tSub Interface Name: %s\n", subIf.getName());
+		}
+	 }
 
-     static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
-        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
-        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
-            out.printf("InetAddress: %s\n", inetAddress);
-        }
-     }
+	 static void displayInterfaceInformation(NetworkInterface netint) throws SocketException {
+		Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+		for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+			out.printf("InetAddress: %s\n", inetAddress);
+		}
+	 }
 
-     static void displayMacAddresses(NetworkInterface netint) throws SocketException {
-        Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
-        for (InetAddress inetAddress : Collections.list(inetAddresses)) {
-            byte[] mac = netint.getHardwareAddress();
-            if (mac != null){
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < mac.length; i++) {
-                sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));        
-            }
-                out.printf("Mac Address: %s\n", sb.toString());
-            }
-        }
-        out.printf("\n");
-     }
+	 static void displayMacAddresses(NetworkInterface netint) throws SocketException {
+		Enumeration<InetAddress> inetAddresses = netint.getInetAddresses();
+		for (InetAddress inetAddress : Collections.list(inetAddresses)) {
+			byte[] mac = netint.getHardwareAddress();
+			if (mac != null){
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < mac.length; i++) {
+				sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));        
+			}
+				out.printf("Mac Address: %s\n", sb.toString());
+			}
+		}
+		out.printf("\n");
+	 }
 
 
 }  
---
+++ Lab 8-9/task 1/ConnectURL_test.java
@@ -0,0 +1,50 @@
+import java.net.*;
+import java.io.*;
+
+public class ConnectURL_test {
+  public static void main(String[] args){
+    URL url = null;
+    try {
+      //try block creating URL from the string given
+      url = new URL("http://m1-c45n1.csit.rmit.edu.au/~Course/index.php");
+    } catch (MalformedURLException e) {
+      //message in case try block has failed
+      System.out.println("Failed to create URL!");
+    }
+
+    //main try block
+    try {
+      //openning connection from the url
+      URLConnection urlConnection = url.openConnection();
+      //getting input stream
+      InputStream input = urlConnection.getInputStream();
+      //getting content type
+      String contentType = urlConnection.getContentType();
+      //getting content length
+      int contentLength = urlConnection.getContentLength();
+      //printing retrieved data
+      System.out.printf("Content type: %s, content length: %d.\n", contentType, contentLength);
+      
+      //creating an object from url content
+      Object content = url.getContent();
+      //check if the object created is of instance InputStream
+      if (content instanceof InputStream) {
+
+        int data;
+        input = (InputStream) content;
+        //loop till there's more data
+        while ((data = input.read()) != -1) {
+          //print the content to the screen
+          System.out.print((char) data);
+        }
+        //notify the user that end of the stream is reached
+        System.out.println("\nInputStreamEnd");
+      }
+      //closing input stream
+      input.close();
+    } catch (IOException e) {
+      //message in case try block has failed
+      System.out.println("Exception was caught: " + e.getClass().getCanonicalName());
+    }
+  }
+}
